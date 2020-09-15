9727a891a476456c886dde80587b1e60bcf96c65
Sai Koka <ssk177@scarletmail.rutgers.edu>
2020-09-15 13:20:51 -0400

    Fizz Buzz Done

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            //TODO:  Implement FizzBuzz
+            for(int i = 1; i <= 100; i++) {
+                if(i%3==0 || i % 5 == 0) {
+                    if(i%3==0 && i % 5 == 0) {
+                        Console.WriteLine(i+" FizzBuzz");
+                    }
+                    else if (i % 5 == 0) {
+                        Console.WriteLine(i + " Buzz");
+                    }
+                    else {
+                        Console.WriteLine(i + " Fizz");
+                    }
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
